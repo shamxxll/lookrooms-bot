e4757caa8cd1060a9d09efe2c9c7ee39cf38797c
shamxxll <shamichdancer0556330340@gmail.com>
2025-07-31 14:30:22 +0300

    Fix: обновлён импорт MemoryStorage для aiogram v3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ﻿from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InputFile
